7ce5456bbddc191057dc2140b49018dcd00197fe
apgaua <apgaua.sousa@gmail.com>
2025-02-06 15:42:48 -0300

    Create iam policy

--- variables.tf
+++ variables.tf
@@ -1,8 +1,24 @@
-variable "service_name" {}
-variable "cluster_name" {}
-variable "vpc_id" {}
-variable "private_subnets" {}
-variable "service_port" {}
-variable "service_cpu" {}
-variable "service_memory" {}
-variable "service_listener" {}
+variable "service_name" {
+    type = string
+}
+variable "cluster_name" {
+    type = string
+}
+variable "vpc_id" {
+    type = string
+}
+variable "private_subnets" {
+    type = list(string)
+}
+variable "service_port" {
+    type = string
+}
+variable "service_cpu" {
+    type = string
+}
+variable "service_memory" {
+    type = string
+}
+variable "service_listener" {
+    type = string
+}
